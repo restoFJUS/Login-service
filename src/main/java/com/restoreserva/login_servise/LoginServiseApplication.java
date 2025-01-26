1f07dfaa94e2a142b60e58f58788524990bcbc03
francomy <francomy554@gmail.com>
2025-01-26 15:16:41 -0300

    Commit inicial

---
+++ src/main/java/com/restoreserva/login_servise/LoginServiseApplication.java
@@ -0,0 +1,13 @@
+package com.restoreserva.login_servise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginServiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restoreserva/login_servise/LoginServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restoreserva.login_servise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
